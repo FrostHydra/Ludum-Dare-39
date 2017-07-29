6bff8bdf58a616fda10d72b2adbb2a59f30a068e
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-07-29 16:29:18 +0200

    Buttons (Text not working)

---
+++ New Unity Project/Assets/Scripts/UIButton/CreateButtonScriptableObject.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+public class CreateButtonScriptableObject {
+
+	[MenuItem("Scriptable Objects/Button")]
+    static void ButtonSO()
+    {
+        ButtonLayout button = new ButtonLayout();
+
+        string path = "Assets/Prefabs/button.asset";
+        path = AssetDatabase.GenerateUniqueAssetPath(path);
+        AssetDatabase.CreateAsset(button, path);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = button;
+    }
+}
+
+
+#endif
---
+++ New Unity Project/Assets/Scripts/UIButton/DrawButtons.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DrawButtons : MonoBehaviour {
+
+    public GUISkin buttonLayout;
+    public string[] buttonName;
+    public bool[] buttonOn;
+
+
+	// Use this for initialization
+	void Start () {
+        buttonOn = new bool[buttonName.Length];
+        for(int i = 0; i < buttonOn.Length; i++)
+        {
+            buttonOn[i] = false;
+        }
+	}
+
+    private void OnGUI()
+    {
+        for (int i = 0; i < buttonName.Length; i++)
+        {
+            int buttonStyle = 1;
+            if (buttonOn[i])
+            {
+                buttonStyle = 0;
+            }
+            if (GUI.Button(new Rect(Screen.width / 20, (Screen.height / (buttonName.Length + 2)) * (i + 1), Screen.width / 6, Screen.height / (buttonName.Length + 2)), buttonName[i], buttonLayout.customStyles[buttonStyle]))
+            {
+                buttonOn[i] = !buttonOn[i];
+                if (buttonOn[i])
+                {
+                    
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
