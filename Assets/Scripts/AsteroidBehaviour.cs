a8b09389af66209fb02dfc0c49493dc36cb95ab2
xMiwah <xMiwah@gmail.com>
2017-07-30 11:53:32 +0200

    asteroids

---
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviour : MonoBehaviour {
+
+    public float speed;
+    public float rotationspeed;
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 position = transform.position;
+        position = new Vector2(position.x, position.y - speed * Time.deltaTime);
+        transform.position = position;
+        transform.Rotate(new Vector3(0, 0, Time.deltaTime * rotationspeed));
+        
+
+        Vector2 end = Camera.main.ViewportToWorldPoint(new Vector2(0, 0));
+
+        if(transform.position.y < end.y )
+        {
+            Destroy(gameObject);
+            print("it's destroyed");
+        }
+
+        
+	}
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+    public Transform[] spawnLocations;
+    public GameObject[] spawnPrefab;
+    public GameObject[] spawnClone;
+    public float spawntime;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        SpawnEnemy();
+	}
+
+
+    public void SpawnEnemy()
+    {
+
+        spawnClone[0] = Instantiate(spawnPrefab[Random.Range(0, 4)], spawnLocations[Random.Range(0,4)].transform.position, Quaternion.identity) as GameObject;
+
+        
+
+    }
+}
+
+
+        //int spawnType = Random.Range(0, 3);
+        //int spawnDirection = Random.Range(0, 5);
+
+        //Vector2 spawnPosition = new Vector2(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y);
+        //print(spawnPosition);
+        //Instantiate(asteroids[spawnType], spawnPosition, Quaternion.identity
+
+        //GameObject newAsteroid = (gameObject)Instantiate(asteroids[randomNumber], spawnLocation);
+
+        //asteroids = asteroids[Random.Range(0, asteroids.Length)];
+
