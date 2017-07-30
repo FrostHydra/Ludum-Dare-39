bd6830b6e50cd6ccd849bfc7334a5a77929080a0
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-07-30 22:31:39 +0200

    Stuff

---
+++ Assets/Scripts/SpaceshipMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipMovement : MonoBehaviour {
+
+    private float posX;
+    public float speed;
+    public float distance;
+    private float sinNr = 0;
+
+    void Start()
+    {
+        posX = this.gameObject.transform.position.y;
+    }
+
+    void Update () {
+        float newPosX = posX + (distance * Mathf.Sin(speed * Time.time));
+        this.transform.position = new Vector3(newPosX, this.transform.position.y, this.transform.position.z);
+	}
+}
