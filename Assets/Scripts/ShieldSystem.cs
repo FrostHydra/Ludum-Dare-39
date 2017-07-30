01b6dd796d91360820dbd8e8ecce75205c0be104
Koenraad <koenraad.kragt@gmail.com>
2017-07-30 22:03:28 +0200

    ShieldSystem

---
+++ Assets/Scripts/ShieldSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldSystem : MonoBehaviour {
+
+    private Renderer rend;
+    private Collider2D coll;
+
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        coll = GetComponent<Collider2D>();
+
+        rend.enabled = false;
+        coll.enabled = false;
+
+    }
+
+    public void powerSystem(bool isPowered)
+    {
+        rend.enabled = isPowered;
+        coll.enabled = isPowered;
+    }
+}
