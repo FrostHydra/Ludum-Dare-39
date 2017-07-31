139b018cb8764dd9ab7389c3effc8a0d68bba74b
Koenraad <koenraad.kragt@gmail.com>
2017-07-31 22:42:42 +0200

    Ship Turrets

---
+++ Assets/Scripts/Shooting/VisualizeTurret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PoweredSystem))]
+public class VisualizeTurret : MonoBehaviour {
+
+    public SpriteRenderer turret;
+    public Color activeColor;
+    public Color inactiveColor;
+
+    public void powerSystem(bool power)
+    {
+        if (power)
+        {
+            turret.color = activeColor;
+        } else
+        {
+            turret.color = inactiveColor;
+
+        }
+    }
+}
