465eb03edc2bd5dd9a8a99b8cc12399d7e5f4e48
Koenraad <koenraad.kragt@gmail.com>
2017-07-31 14:02:45 +0200

    Earth move based on velocity, power cost tweaks

--- Assets/Scripts/Earth.cs
+++ Assets/Scripts/Earth.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class Earth : MonoBehaviour {
 
-    public float speed;
-	// Use this for initialization
+    public float maxSpeed = 0.1f;
+    private float speed;
+
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        speed = (ShipStats.Instance.velocity / ShipStats.Instance.maximumVelocity) * maxSpeed;
+
         Vector2 position = transform.position;
         position = new Vector2(position.x, position.y - speed *Time.deltaTime);
         transform.position = position;
